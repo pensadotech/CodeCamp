b140905630c50f21d2683b72c1b881c1eb638036
apensado <armando@pensadotech.com>
2020-01-26 10:22:44 -0800

    Adding Location delete operation

---
+++ CodeCampApp/CodeCampApp/Pages/Locations/Delete.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain = CodeCamp.Domain;
+using CodeCampApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CodeCampApp.Pages.Locations
+{
+    public class DeleteModel : PageModel
+    {
+        // Private members..........................
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ListModel> _logger;
+        private readonly Domain.Repositories.ICampRepository _campRepository;
+
+        // Properties .....................................
+        public LocationModel Location { get; set; }
+
+        // Constructors ....................................
+        public DeleteModel(IConfiguration config, ILogger<ListModel> logger,
+            IMapper mapper, Domain.Repositories.ICampRepository campRepository)
+        {
+            _config = config;
+            _logger = logger;
+            _mapper = mapper;
+            _campRepository = campRepository;
+        }
+
+        // Methods .........................................
+        public IActionResult OnGet(int locationId)
+        {
+            // Retreive the deail for the selected model 
+            Domain.Entities.Location location = _campRepository.GetLocationById(locationId);
+
+            // Copy data from Domain DTo to App Model
+            Location = _mapper.Map<LocationModel>(location);
+
+            if (Location == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        // In Razor pages, we have two default handlers, OnGet and OnPost
+        // The POTS operation will be used to take a delete action
+        public IActionResult OnPost(int locationId)
+        {
+            // Retreive the deail for the selected model 
+            Domain.Entities.Location location = _campRepository.GetLocationById(locationId);
+
+            // Retreive the deail for the selected model 
+            location = _campRepository.Delete<Domain.Entities.Location>(location);
+
+            // Commit the operation
+            this._campRepository.CommitChanges();
+
+            // Copy data from Domain DTo to App Model
+            Location = _mapper.Map<LocationModel>(location);
+
+            // Send to the paget that will be redirected, a message through TempData
+            // to inform the user about the operation that took place. 
+            TempData["ActionMessage"] = $"{Location.VenueName} deleted";
+
+            // IMPORTANT: For ADD, EDIT, or DELETE operations allwyas redirect to a different page
+            // Force re-direction to detail page, do not stay in curent page
+            return RedirectToPage("./List");
+        }
+    }
+}
